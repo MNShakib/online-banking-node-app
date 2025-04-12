f68d1fe9cd9f3882b2448fba37afaf7456c063c0
MNShakib <mnshakib79@gmail.com>
2025-04-12 09:09:58 +0530

    update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,3 @@
-// // middlewares/auth.js
-// module.exports.ensureLoggedIn = function(req, res, next) {
-//     if (req.session.userId) {
-//         // If user is logged in (session contains userId), proceed
-//         next();
-//     } else {
-//         // Not logged in – redirect to login page
-//         req.flash('error', 'Please log in to continue');
-//         res.redirect('/login');
-//     }
-// };
-
-// module.exports.ensureAdmin = function(req, res, next) {
-//     if (req.session.userId && req.session.role === 'admin') {
-//         next();
-//     } else {
-//         // Forbidden – if logged in as non-admin, or not logged in at all
-//         res.status(403);
-//         return res.render('error.ejs', { message: "Access denied. Admins only." });
-//     }
-// };
 // middlewares/auth.js
 module.exports.ensureLoggedIn = function(req, res, next) {
     if (req.session.userId) {
